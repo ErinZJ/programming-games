1fbe5d89054f5d38d0c707becbeac5243a1f5079
Erin <moon4elf20@gmail.com>
2022-05-06 17:11:55 +0200

    flip game

--- cypress/integration/puzzleGame.spec.ts
+++ cypress/integration/puzzleGame.spec.ts
@@ -3,11 +3,11 @@
 describe("PuzzleGame page", () => {
   beforeEach(() => {
     cy.visit("http://localhost:3000/puzzle-game");
-  });
+  }); 
   it("is card clickable and card moves", () => {
-    cy.get('[data-clickable="true"]:first').then(($nav) => {
-      const x =$nav.data().x
-      const y =$nav.data().y
+    cy.get('[data-clickable="true"]:first').then((card) => {
+      const x =card.data().x
+      const y =card.data().y
       cy.get('[data-clickable="true"]:first').click();
       cy.get(`[data-x=${x}][data-y=${y}].empty-card`).should("exist",true)
     }) ;
--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import { BlackJack } from './blackJack';
 import { PuzzleGame } from './puzzleGame/puzzle';
+import { FlipGame } from "./flipGame/flipGame";
 import { Home } from './home';
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
    <Routes>
         <Route path="blackjack" element={<BlackJack />} />
         <Route path="puzzle-game" element={<PuzzleGame />} />
+        <Route path="flip-Game" element={<FlipGame />} />
         <Route path="/" element={<Home />} />
       </Routes> 
     </div>
